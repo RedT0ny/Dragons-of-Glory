060fb47a57fc63ec3ee6972c2a134ea1ce9f0cfd
RedT0ny <155879855+RedT0ny@users.noreply.github.com>
2025-08-26 00:12:36 +0200

    Enhance Country class constructor with additional attributes for capital, allegiance, and activation

--- src/game/country.py
+++ src/game/country.py
@@ -1,7 +1,11 @@
 class Country:
-    def __init__(self, name, color, is_ai=False):
+    def __init__(self, name, color, capital, allegiance, activation, is_ai=False):
         self.name = name
         self.color = color  # For map rendering
+        self.capital = capital
+        self.allegiance = allegiance # Whitesone, Highlord, neutral
+        #self.alignment = (none,none) # (WS, HL) activation bonus/malus only for basic rules
+        self.activation = activation # Activation bonus for the country
         self.is_ai = is_ai
         self.units = []
         self.territories = []
