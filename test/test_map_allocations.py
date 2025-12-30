3fd50abebbeda74d84e836523e1695d9a82562c4
RedT0ny <155879855+RedT0ny@users.noreply.github.com>
2025-12-30 18:45:56 +0100

    Fixing wrong territories of countries. Updating test script to show locations.

--- test/test_map_allocations.py
+++ test/test_map_allocations.py
@@ -27,6 +27,31 @@ def draw_hexagon(surface, color, center, radius):
     pygame.draw.polygon(surface, color, points)
     pygame.draw.polygon(surface, (40, 40, 40), points, 1) # Hex border
 
+def draw_location_symbol(surface, center, loc_type, is_capital):
+    """Draws a symbol representing the type of location."""
+    size = 6
+    if loc_type == 'city':
+        # Square for cities
+        rect = pygame.Rect(center[0] - size//2, center[1] - size//2, size, size)
+        pygame.draw.rect(surface, (255, 255, 255), rect)
+    elif loc_type == 'fortress':
+        # Diamond for fortresses
+        pts = [(center[0], center[1] - size), (center[0] + size, center[1]), 
+               (center[0], center[1] + size), (center[0] - size, center[1])]
+        pygame.draw.polygon(surface, (255, 255, 255), pts)
+    elif loc_type == 'port':
+        # Circle for ports
+        pygame.draw.circle(surface, (255, 255, 255), center, size // 2 + 1)
+    else:
+        # Triangle for undercities/others
+        pts = [(center[0], center[1] - size), (center[0] - size, center[1] + size), 
+               (center[0] + size, center[1] + size)]
+        pygame.draw.polygon(surface, (255, 255, 255), pts)
+    
+    if is_capital:
+        # Gold dot on top for capitals
+        pygame.draw.circle(surface, (255, 215, 0), (center[0], center[1] - size - 2), 2)
+
 def run_visualization():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -37,18 +62,37 @@ def run_visualization():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     yaml_path = os.path.join(current_dir, "..", "data", "countries.yaml")
     
-    with open(yaml_path, 'r') as f:
-        countries_data = yaml.safe_load(f)
+    try:
+        with open(yaml_path, 'r') as f:
+            countries_data = yaml.safe_load(f)
+    except FileNotFoundError:
+        print(f"Error: Could not find {yaml_path}. Check your directory structure.")
+        return
 
-    # Map hexes to colors based on country data
+    # Map hexes to colors and store locations
     hex_map = {}
+    location_map = {} # (col, row) -> {type, is_capital}
+    
     for cid, data in countries_data.items():
         color_hex = data.get('color', '#808080').lstrip('#')
         color_rgb = tuple(int(color_hex[i:i+2], 16) for i in (0, 2, 4))
-        
+        capital_id = data.get('capital_id')
+    
+        # Load territories
         for coord in data.get('territories', []):
             if isinstance(coord, list) and len(coord) == 2:
                 hex_map[tuple(coord)] = color_rgb
+            elif isinstance(coord, list) and len(coord) == 1: # Handle edge case [31, 13]
+                pass # Handled by the check above usually, but YAML can be tricky
+        
+        # Load locations
+        for loc_id, loc_info in data.get('locations', {}).items():
+            coords = tuple(loc_info.get('coords', []))
+            if coords:
+                location_map[coords] = {
+                    'type': loc_info.get('type', 'city'),
+                    'is_capital': (loc_id == capital_id)
+                }
 
     running = True
     while running:
@@ -62,10 +106,15 @@ def run_visualization():
         for row in range(MAP_HEIGHT):
             for col in range(MAP_WIDTH):
                 center = get_hex_center(col, row, HEX_RADIUS)
-                
+            
                 # Use country color if allocated, otherwise use a faint gray for empty hexes
                 color = hex_map.get((col, row), (50, 50, 50))
                 draw_hexagon(screen, color, center, HEX_RADIUS)
+                
+                # Draw location symbol if present
+                loc = location_map.get((col, row))
+                if loc:
+                    draw_location_symbol(screen, center, loc['type'], loc['is_capital'])
 
         pygame.display.flip()
         clock.tick(30)
